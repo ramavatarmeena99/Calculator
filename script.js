c4d77809b04c3b469cb5b3b6c408dada52d04680
ramavatarmeena99 <meenaramavatar48@gmail.com>
2021-12-28 20:20:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const input_box = document.querySelector(".calc")
+const clearbutton = document.querySelector(".clear")
+
+clearbutton.addEventListener("click" ,()=>{
+    input_box.value = " ";
+     })
+    
